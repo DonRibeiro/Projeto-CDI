ebe86174319f1e7cd50b8978d13152fa6362abcc
DonRibeiro <donavan.assis@gmail.com>
2025-04-26 15:59:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ buttonC.addEventListener("click", calcular) // Adiciona ação de calcular
 
 function calcular() {
   
-    let invest = document.getElementById("invest") // Integrar com a DOM
-    let perio = document.getElementById('perio')
-    let taxa = document.getElementById('taxa')
-    let cdi = document.getElementById('cdi')
-    let res = document.getElementById('res')
+    const invest = document.getElementById("invest") // Integrar com a DOM
+    const perio = document.getElementById('perio')
+    const taxa = document.getElementById('taxa')
+    const cdi = document.getElementById('cdi')
+    const res = document.getElementById('res')
   
     const investVal = Number(invest.value) //Inverter a string para number
     const perioVal = Number(perio.value)
     const taxaVal = Number(taxa.value)
     const cdiVal = Number(cdi.value)
     if(invest.value.length == 0|| invest.value.length == 0 || taxa.value.length == 0|| cdi.value.length == 0) {
-        window.alert("Erro ao executar, por favor informe todos os valores.") }
+        window.alert("Erro ao executar, por favor informe todos os valores.") } // Erro caso não seja digitado nenhum valor.
     else {
       
-      let rend = investVal * (cdiVal/100) * (taxaVal/100) //Cálculo para o rendimento
+      const rend = investVal * (cdiVal/100) * (taxaVal/100) //Cálculo para o rendimento
         
-        let valorf = investVal * (1 + taxaVal/100)**perioVal //Juros compostos para descobrir o montante
+        const valorf = investVal * (1 + taxaVal/100)**perioVal //Juros compostos para descobrir o montante
         
         let rendB = valorf - investVal
        
        let total= rendB + investVal
        
        
-       let convert = perioVal * 365
+       const convert = perioVal * 365
        //Conversão de ano para dia
        
        
